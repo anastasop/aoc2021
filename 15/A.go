064e790c04772261b08689763ff8d758f34ddfe2
Spyros Anastasopoulos <winwasher@gmail.com>
2021-12-15 20:32:32 +0200

    Day 15

---
+++ 15/A.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type point struct {
+	x, y int
+	dist int
+}
+
+var grid [][]byte
+
+func main() {
+	inp, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "IO error: %v\n", err)
+		os.Exit(2)
+	}
+
+	lines := strings.Split(string(inp), "\n")
+	lines = lines[:len(lines)-1]
+	grid = make([][]byte, len(lines))
+	for y, line := range lines {
+		grid[y] = make([]byte, len(line))
+		for x, c := range line {
+			grid[y][x] = byte(c - '0')
+		}
+	}
+
+	search()
+}
+
+func inGrid(x, y int) bool {
+	return 0 <= y && y < len(grid) && 0 <= x && x < len(grid[0])
+}
+
+func search() {
+	var q []*point
+	var e []*point
+
+	q = append(q, &point{0, 0, 0})
+	for len(q) > 0 {
+		var top *point
+
+		q, top = pointMinDist(q)
+		e = add(e, top.x, top.y, top.dist)
+
+		if top.x == len(grid)-1 && top.y == len(grid)-1 {
+			break
+		}
+
+		check := func(x, y int) {
+			if inGrid(x, y) {
+				d := top.dist + int(grid[y][x])
+				t0, t1 := find(q, x, y), find(e, x, y)
+				if t0 == nil && t1 == nil {
+					q = add(q, x, y, d)
+				} else if t0 != nil && d < t0.dist {
+					t0.dist = d
+				}
+			}
+		}
+
+		x, y := top.x, top.y
+		check(x, y-1)
+		check(x, y+1)
+		check(x-1, y)
+		check(x+1, y)
+	}
+
+	fmt.Println(e[len(e)-1].dist)
+}
+
+func pointMinDist(q []*point) ([]*point, *point) {
+	i := 0
+	p := q[0]
+	for k, t := range q {
+		if t.dist < p.dist {
+			i = k
+			p = t
+		}
+	}
+	q[i] = q[len(q)-1]
+	q = q[:len(q)-1]
+	return q, p
+}
+
+func find(q []*point, x, y int) *point {
+	for _, t := range q {
+		if t.x == x && t.y == y {
+			return t
+		}
+	}
+	return nil
+}
+
+func add(q []*point, x, y, dist int) []*point {
+	return append(q, &point{x, y, dist})
+}
